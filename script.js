e6dd511cafbf53c0c04d6f1a4560896d85b5fbf9
Bruno Jacby <brunojck@hotmail.com>
2023-01-01 17:20:29 +0000

    atualizaçao de componentes

---
+++ script.js
@@ -0,0 +1,10 @@
+function removerdd() {
+    document.getElementById('infos').classList.remove('disabled') 
+    document.getElementById('homeText').classList.add('disabled')
+    document.getElementById('project').classList.add('disabled')      
+}
+
+function projetos() {
+    document.getElementById('project').classList.remove('disabled') 
+    document.getElementById('homeText').classList.add('disabled')     
+}
